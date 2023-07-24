a7157a59c3b6904904009fccf97ab417c82f2425
Lainlnya <kimyelim100@gmail.com>
2023-07-25 00:00:01 +0900

    [solved] BOJ 3985

--- _3985.java
+++ _3985.java
@@ -1,34 +1,50 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class _3985 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int[] N = new int[Integer.parseInt(br.readLine()) + 1];
-		
+		int N = Integer.parseInt(br.readLine());
+		ArrayList list1 = new ArrayList<>();
+
+		for(int i = 0; i <= N; i++) {
+			list1.add(0);
+		}
+
 		int M = Integer.parseInt(br.readLine());
-		int max = 0, count = 0;
-		
+		int max = 0, count = 0, maxP = 0, countP = 0, temp = 0;
+
 		for (int j = 0; j < M; j++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 
-			if (b - a > max) max = b - a;
-			
+			if (b - a + 1 > max) {
+				max = b - a + 1;
+				maxP = j + 1;
+			};
+
 			int k = a - 1;
-			while (k++ < b) {
-				if (N[k] == 0) {
-					N[k] = j + 1;
+			while (k < b) {
+				if (list1.get(k).equals(0)) {
+					list1.remove(k);
+					list1.add(k, j + 1);
 				}
+				k++;
 			}
 		}
-		
-		for (int i = 0; i < M; i++) {
+
+		for (int i = M; i > 0; i--) {
+			temp = Collections.frequency(list1, i);
+			if (temp >= count) {
+				count = temp;
+				countP = i;
+			}
 		}
-		
-		System.out.println(max);
-		System.out.println(Arrays.toString(N));
+
+		System.out.println(maxP);
+		System.out.println(countP);
 	}
 }
