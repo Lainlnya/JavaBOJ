4334c363a4d1c8ad0f0aa7129186e574a66b747b
Lainlnya <kimyelim100@gmail.com>
2023-08-01 00:21:53 +0900

    [solved] BOJ 13251

--- _13251.java
+++ _13251.java
@@ -6,50 +6,35 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class _13251 {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int M = Integer.parseInt(br.readLine());
+		int[] arr = new int[M];
+		int total = 0, K = 0;
 		double answer = 0;
-		double answerUp = 0;
-		double total = 0;
-		if (M == 1) {
-			answer = 1;
-			System.out.println(answer);
-		} else {
-			List<Integer> stone = new ArrayList<>();
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < M; i++) {
-				stone.add(Integer.parseInt(st.nextToken()));
-			}
-			
-			System.out.println(stone);
-			int N = Integer.parseInt(br.readLine());
-			int copyN = N;
-			if (N != 1) {
-				for (int j = 0; j < stone.size(); j++) {
-					double temp = 1;
-					int K = stone.get(j);
-					total += K;
-					while (copyN-- > 0 && K > N) {
-						temp *= (K--);
-						System.out.println(temp);
-					}
-					copyN = N;
-					answerUp += temp;
-					temp = 1;
-				}
-				
-				for (int j = 0; j < N - 1; j++) {
-					total *= (--total);
-				}
-				answer = answerUp / total;
-				System.out.println(answer);
-			} else {
-				answer = 1;
-				System.out.println(answer);
-			}
-			
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < M; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			total += arr[i];
 		}
-		
+
+		K = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < M; i++) {
+			if (arr[i] >= K) {
+				int temp = total;
+				int temp2 = K;
+
+				double tempAnswer = 1;
+				while (temp2-- > 0) {
+					tempAnswer *= (arr[i]-- / (double) temp--);
+				}
+				answer += tempAnswer;
+			}
+		}
+		System.out.println(answer >= 1 ? 1 : answer);
+
 	}
 }
