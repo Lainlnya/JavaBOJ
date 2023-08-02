ef3153ee37e7810f9d4db2755c511025dbdf3996
Lainlnya <kimyelim100@gmail.com>
2023-08-02 10:13:45 +0900

    [pending] BOJ 10262

--- _10262.java
+++ _10262.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -12,8 +11,7 @@ public class _10262 {
 		List<Integer> ggung = new ArrayList<>();
 		List<Integer> seok = new ArrayList<>();
 		int K = 2;
-		double result = 0.0;
-		double tempBig = 0;
+		int ggungR = 0, seokR = 0;
 		while (K-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
@@ -23,44 +21,22 @@ public class _10262 {
 			
 			for(int i = a; i <= b; i++) {
 				for (int j = c; j <= d; j++) {
-					if (K == 1) ggung.add(i + j);
-					else seok.add(i + j);
+					if (K == 1) {
+						if (!ggung.contains(i + j)) ggung.add(i + j);
+					}
+					else {
+						if (!seok.contains(i + j)) seok.add(i + j);
+					}
 				}
 			}
 		}
 		
-		Collections.sort(ggung);
-		Collections.sort(seok);
+		ggungR = ggung.get(0) + ggung.get(ggung.size() - 1);
+		seokR = seok.get(0) + seok.get(seok.size() - 1); 
 
-		System.out.println(ggung);
-		System.out.println(seok);
-		for (int i = seok.get(0); i <= seok.get(seok.size() - 1); i++) {
-			double temp = 0;
-			double tempSeok = Collections.frequency(seok, i);
-
-			for (int j = i; j <= ggung.get(ggung.size() - 1); j++) {
-				if(j >= seok.get(0)) {
-					double tempGgng = Collections.frequency(ggung, j);
-					temp += tempGgng;
-				}
-
-				if (j > seok.get(seok.size() - 1)) {
-					tempBig += Collections.frequency(ggung, j);
-				}
-			}
-			result += (tempSeok * temp);
-		}
-		tempBig /= (seok.get(seok.size() - 1) - seok.get(0) + 1);
-
-		result -= (tempBig * ggung.size());
-		System.out.println(result);
-
-		result /= (seok.size() * ggung.size());
-
-		System.out.println(result);
-		if (result > 0.5) {
+		if (ggungR > seokR) {
 			System.out.println("Gunnar");
-		} else if (result < 0.5) {
+		} else if (ggungR < seokR) {
 			System.out.println("Emma");
 		} else {
 			System.out.println("Tie");
