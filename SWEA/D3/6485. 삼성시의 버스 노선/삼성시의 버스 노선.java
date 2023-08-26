b6e9bea260934bed3e113987b630b141667a41ad
Lainlnya <93235981+Lainlnya@users.noreply.github.com>
2023-08-26 19:59:50 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 194 ms, Memory: 36,112 KB -BaekjoonHub

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.java
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.java
@@ -1,34 +1,42 @@
-// package SWEA_6485_삼성시;
+//package SWEA_6485_삼성시의_버스_노선;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt(); // 전체 테스트케이스 개수
-		
-		for (int tc = 0; tc < T; tc++) {
-			int[] busStop = new int[5001]; // 최대 개수 500이므로 501짜리 int배열 선언
-			int busCase = sc.nextInt(); // 버스 개수
-			
-			// 버스 지나는 노선 추가하기
-			for (int b = 0; b < busCase; b++) {
-				int start = sc.nextInt();
-				int stop = sc.nextInt();
-				
-				for (int i = start; i <= stop; i++) {
-					busStop[i]++;
-				}
-			}
-			
-			int where = sc.nextInt();
-			
-			// 출력부
-			System.out.print("#" + (tc + 1) + " ");
-			for (int i = 0; i < where; i++) {
-				System.out.print(busStop[sc.nextInt()] + " ");
-			}
-			System.out.println();
-		}
-	} // 메인 끝
-}
+    public static int[] busStop = new int[5001];
+
+    public static void main(String[] args) throws FileNotFoundException {
+//        System.setIn(new FileInputStream("src/SWEA_6485_삼성시의_버스_노선/s_input.txt"));
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        for(int tc = 1; tc <= T; tc++) {
+            busStop = new int[5001];
+            //버스노선 개수
+            int N = sc.nextInt();
+
+            for(int i = 0; i < N; i++) {
+                //지나가는 버스정류장의 최소 번호
+                int A = sc.nextInt();
+                //지나가는 버스정류장의 최대 번호
+                int B = sc.nextInt();
+                //해당 범위를 지나가므로 busStop의 인덱스를 이용하여 지나가는 버스 수 증가
+                for(int j = A; j <= B; j++) {
+                    busStop[j]++;
+                }
+            }
+
+            int P = sc.nextInt();
+            System.out.print("#" + tc + " ");
+            for(int i = 1; i <= P; i++) {
+                int C = sc.nextInt();
+                System.out.print(busStop[C] + " ");
+            }
+            System.out.println();
+
+        }//tc
+    }//main
+}//class
