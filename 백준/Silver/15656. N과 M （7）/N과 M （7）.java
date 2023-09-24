80d461c9b63ebb0cc355b426122295e2db396a84
Lainlnya <93235981+Lainlnya@users.noreply.github.com>
2023-09-24 17:20:29 +0900

    [Silver III] Title: N과 M (7), Time: 708 ms, Memory: 368184 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    // 중복순열
+    static int N; // 자연수 개수
+    static int M; // 고를 개수
+    static int[] arr; // 주어진 배열
+    static int[] answer; // 만들 배열
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        answer = new int[M];
+        sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+        if (M == depth) {
+            for (int value: answer) {
+                sb.append(value + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            answer[depth] = arr[i];
+            dfs(depth + 1);
+
+        }
+    }
+}
