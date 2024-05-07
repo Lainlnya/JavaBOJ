d594702019b7e9d1f67385c24fa30e28ca5e9b5a
Lainlnya <93235981+Lainlnya@users.noreply.github.com>
2024-05-08 00:08:37 +0900

    [Silver IV] Title: 카드2, Time: 140 ms, Memory: 43028 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int lastNum = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= lastNum; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() != 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.peek());
+    }
+}
