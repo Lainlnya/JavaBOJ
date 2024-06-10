cc50363c060d0d88235cae8483e3d629f852d97d
Lainlnya <93235981+Lainlnya@users.noreply.github.com>
2024-06-11 01:28:15 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 532 ms, Memory: 292260 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -2,29 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    // 1, 2, 3, 4, 5, 6, 7
+    // 순서대로 k번쨰 사람 제거
+    // 결과 3, 6, 2, 7, 5, 1, 4
+
+    // 앞과 뒤가 연결되어있는 자료구조여야하므로, 큐를 사용하는 것이 나을 것 같다.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        int total = Integer.parseInt(st.nextToken());
-        int term = Integer.parseInt(st.nextToken());
+        String command = br.readLine();
+        int person = Integer.parseInt(command.split(" ")[0]);
+        int cut = Integer.parseInt(command.split(" ")[1]);
+
         Queue<Integer> queue = new LinkedList<>();
 
-        // 일단 list로 숫자에 맞게 넣기
-        for (int i = 1; i <= total; i++) {
+        for (int i = 1; i <= person; i++) {
             queue.add(i);
         }
-
-        int count = 1;
         sb.append("<");
+
         while (!queue.isEmpty()) {
-             if (count++ == term) {
-                 if (queue.size() == 1) sb.append(queue.poll() + ">");
-                 else sb.append(queue.poll() + ", ");
-                 count = 1;
-             } else queue.add(queue.poll());
+            for (int i = 1; i <= cut; i++) {
+                if (i == cut) {
+                    sb.append(queue.poll());
+                } else queue.add(queue.poll());
+            }
+
+            if (!queue.isEmpty()) sb.append(", ");
         }
+        sb.append(">");
         System.out.println(sb);
     }
+
 }
