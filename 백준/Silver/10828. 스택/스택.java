6c1bd58cd05d628a6c34d719ab2cc6dc4df2a015
Lainlnya <93235981+Lainlnya@users.noreply.github.com>
2024-06-10 17:49:22 +0900

    [Silver IV] Title: 스택, Time: 156 ms, Memory: 17264 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,41 +1,49 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
+// 스택 기본 문제
 public class Main {
-    static Stack<Integer> stack;
-    public static void main(String[] args)  {
-        Scanner sc = new Scanner(System.in);
+    static ArrayList<Integer> stack;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 문자열을 쪼개기 위한 StringTokenizer
+        StringTokenizer st = null;
         StringBuilder sb = new StringBuilder();
-        stack = new Stack<Integer>();
-        int inputN = sc.nextInt();
-        int i = 0;
-        while (i++ < inputN) {
-            switch (sc.next()) {
+        stack = new ArrayList<>();
+
+        int totalN = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < totalN; i++) {
+            st = new StringTokenizer(br.readLine());
+            String input =  st.nextToken();
+
+            switch (input) {
+                // push X: 정수 X를 스택에 넣는 연산
                 case "push":
-                    stack.push(sc.nextInt());
+                    stack.add(0, Integer.parseInt(st.nextToken()));
                     break;
-                case "top":
-                    if (stack.isEmpty()) {
-                        sb.append(-1 + "\n");
-                    } else sb.append(stack.peek() + "\n");
+
+                // pop: 가장 위에 있는 숫자를 뺴고 출력, 바어있다면 -1 리턴
+                case "pop":
+                    sb.append((stack.isEmpty() ? -1 : stack.remove(0)) + "\n");
                     break;
+
+                // size: 스택에 들어있는 정수의 개수
                 case "size" :
                     sb.append(stack.size() + "\n");
                     break;
-                case "pop":
-                    if (stack.isEmpty()) {
-                        sb.append(-1 + "\n");
-                    } else sb.append(stack.pop() + "\n");
 
-                    break;
+                // empty: 스택이 비어있으면 1 아니면 0
                 case "empty":
-                    if (stack.isEmpty()) {
-                        sb.append(1 + "\n");
-                    } else sb.append(0 + "\n");
+                    sb.append((stack.isEmpty() ? 1 : 0) + "\n");
+                    break;
+
+                // top: 스택의 가장 위에 있는 정수 출력, 없으면 -1 리턴
+                case "top":
+                    sb.append((stack.isEmpty() ? -1 : stack.get(0)) + "\n");
                     break;
             }
         }
-
         System.out.println(sb);
     }
 }
