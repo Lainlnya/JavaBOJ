f7e64dc2d819cd5dba2178d821300b7ff507e282
Lainlnya <93235981+Lainlnya@users.noreply.github.com>
2024-06-15 21:20:34 +0900

    [level 3] Title: 입국심사, Time: 0.06 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,33 +1,33 @@
+import java.util.*;
+
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
-
         long start = 1, end = Long.MIN_VALUE;
         for (int time : times) {
             end = Math.max(time, end);
         }
-
+        
         end = end * n;
-
+        
         while (start <= end) {
-            long people = 0;
             long mid = (start + end) / 2;
-
-            for (int i = 0; i < times.length; i++) {
-                people += mid / times[i];
-
-                if (people >= n) break;
+            long pass = 0;
+            
+            for (int time : times) {
+                pass += (mid / time);
+                if (pass >= n) break;
             }
-
-
-            if (people >= n) {
+            
+            if (pass >= n) {
                 answer = mid;
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
+        
         return answer;
+        
     }
-
 }
