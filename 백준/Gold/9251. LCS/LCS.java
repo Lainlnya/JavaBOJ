422f82dcf4b456f64a5bfcc79747e10f7e6aa0d8
Lainlnya <93235981+Lainlnya@users.noreply.github.com>
2024-08-27 02:39:44 +0900

    [Gold V] Title: LCS, Time: 92 ms, Memory: 15928 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr1 = br.readLine().toCharArray();
+        char[] arr2 = br.readLine().toCharArray();
+
+        int[][] dp = new int[arr2.length + 1][arr1.length + 1];
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (arr1[j - 1] == arr2[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[arr2.length][arr1.length]);
+    }
+}
