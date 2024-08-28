849783810a3d8759eb125d87ac56372b88ae8095
Lainlnya <93235981+Lainlnya@users.noreply.github.com>
2024-08-29 03:28:10 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 148 ms, Memory: 15816 KB -BaekjoonHub

---
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] intArr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < intArr.length; i++) {
+            intArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int M = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int gender = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            if (gender == 1) {
+                for (int j = 0; j < N; j++) {
+                    if ((j + 1) % num == 0) {
+                        intArr[j] = (intArr[j] == 1) ? 0 : 1;
+                    }
+                }
+            } else {
+                intArr[num - 1] = (intArr[num - 1] == 1) ? 0 : 1;
+
+                for (int j = 1; j < N / 2; j++) {
+                    if (num - 1 + j >= N || num - 1 - j < 0) {
+                        break;
+                    }
+
+                    if (intArr[num - 1 + j] == intArr[num - 1 - j]) {
+                        intArr[num - 1 + j] = (intArr[num - 1 + j] == 1) ? 0 : 1;
+                        intArr[num - 1 - j] = (intArr[num - 1 - j] == 1) ? 0 : 1;
+                    } else break;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(intArr[i] + " ");
+            if ((i + 1) % 20 == 0) {
+                System.out.println();
+            }
+        }
+
+    }
+}
