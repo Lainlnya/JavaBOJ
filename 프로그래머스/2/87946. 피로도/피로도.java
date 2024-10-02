f5ffac535517d6ef8afb8b87eea82b3fb46d3b5e
Lainlnya <93235981+Lainlnya@users.noreply.github.com>
2024-10-02 09:47:52 +0900

    [level 2] Title: 피로도, Time: 7.76 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -7,43 +7,30 @@
 import java.util.*;
 
 class Solution {
-    static ArrayList<int[][]> arr;
     static int[][] combi;
     static boolean[] visited;
+    static int max = Integer.MIN_VALUE;
 
     public int solution(int k, int[][] dungeons) {
-        int max = Integer.MIN_VALUE;
-        arr = new ArrayList<>();
         combi = new int[dungeons.length][1];
         visited = new boolean[dungeons.length];
-        dfs(0, dungeons);
-        
-        int size = arr.size();
-        
-        for (int[][] dungeon : arr) {
-            int limit = k;
-            int count = 0;
-            aa: for (int j = 0; j < dungeons.length; j++) {
-                if (limit >= dungeon[j][0]) {
-                    limit -= dungeon[j][1];
-                    count++;
-                } else break aa;
-            }
-            System.out.println(count);
-            max = Math.max(count, max);
-        }
+        dfs(0, dungeons, k);
         
         return max;
     }
     
-    public static void dfs(int depth, int[][] dungeons) {
+    public static void dfs(int depth, int[][] dungeons, int k) {
         if (dungeons.length == depth) {
-            int[][] dungeon = new int[depth][1];
-            
-            for (int i = 0; i < depth; i++) {
-                dungeon[i] = combi[i];
+            int limit = k;
+            int count = 0;
+            for (int[] dungeon: combi) {
+                if (limit >= dungeon[0]) {
+                    limit -= dungeon[1];
+                    count++;
+                } else break;
             }
-            arr.add(dungeon);
+            
+            max = Math.max(count, max);
             return;
         }
         
@@ -51,7 +38,7 @@ class Solution {
             if (!visited[i]) {
                 visited[i] = true;
                 combi[depth] = dungeons[i];    
-                dfs(depth + 1, dungeons);
+                dfs(depth + 1, dungeons, k);
                 visited[i] = false;
             }
 
