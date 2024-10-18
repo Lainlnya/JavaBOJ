d7d02e58cf53b343f07dab3ea88a65d93e9aed70
Lainlnya <93235981+Lainlnya@users.noreply.github.com>
2024-10-18 20:52:04 +0900

    [level 4] Title: 징검다리, Time: 41.30 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -6,16 +6,11 @@ class Solution {
         int answer = Integer.MIN_VALUE;
         
         int[] total = new int[rocks.length + 2];
-        int[] gap = new int[total.length];
         
         for (int i = 1; i < total.length - 1; i++) {
             total[i] = rocks[i - 1];
         }
         total[rocks.length + 1] = distance;
-        
-        for (int i = 1; i < total.length; i++) {
-            gap[i] = total[i] - total[i - 1];
-        }
         
         int start = 0, end = distance;
         int mid = 0;
@@ -26,10 +21,8 @@ class Solution {
             mid = (start + end) / 2;
             
             aa: for (int i = 1; i <= total.length - 1; i++) {
-                // mid 보다 작으면 제거
                 if (mid > total[i] - total[idx]) {
                     count++;
-                    // 작지 않으면 다음거 계산
                 } else {
                     idx = i;
                 }
